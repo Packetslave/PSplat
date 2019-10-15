d3e3e657fc1239af1114e66de413236ac13d078c
Brian Landers <blanders@arya.local>
2019-10-14 20:33:42 -0700

    setup build for GH pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,11 +4,11 @@ const path = require('path');
 const config = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './dist'
+    contentBase: './docs'
   },
   module: {
     rules: [
